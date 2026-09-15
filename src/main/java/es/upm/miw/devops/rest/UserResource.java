49b810a4655fc8203fc5830b0f8a022b06ff7740
rpinedaba <re.pineda@alumnos.upm.es>
2026-09-15 23:00:00 +0200

    feature3: add delete user endpoint. Refs #7

--- src/main/java/es/upm/miw/devops/rest/UserResource.java
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -3,6 +3,7 @@ package es.upm.miw.devops.rest;
 import es.upm.miw.devops.models.User;
 import es.upm.miw.devops.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,10 @@ public class UserResource {
     @GetMapping(UserResource.ID_ID)
     public User read(@PathVariable String id) {
         return this.userService.read(id);
+    }
+
+    @DeleteMapping(UserResource.ID_ID)
+    public void delete(@PathVariable String id) {
+        this.userService.delete(id);
     }
 }
